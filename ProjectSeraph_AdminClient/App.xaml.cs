26bbfe67792b9a5061c8dcddb77f1a9629ab42b9
Niklas <jan-niklas-bremer@web.de>
2025-11-27 09:42:55 +0100

    Add project files.

---
+++ ProjectSeraph_AdminClient/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ProjectSeraph_AdminClient
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
