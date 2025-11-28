0429d6f82ade425e610f5787d746f71ff6be1c3a
Jepasej <146959451+Jepasej@users.noreply.github.com>
2025-11-28 10:37:57 +0100

    finished bloodsugar entering

--- projectseraph.client/src/presentation/App.tsx
+++ projectseraph.client/src/presentation/App.tsx
@@ -1,13 +1,25 @@
 ﻿import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header.tsx';
-import MeasurementButton from './components/MeasurementButton.tsx';
+import BloodSugarForm from './components/BloodSugarForm.tsx';
+import Home from './components/Home.tsx';
+import NotFound from './components/NotFound.tsx';
 
+//mellem header og routes skal vi have nogle if/conditional rendering paa:
+//1: er det en nurse eller en borger?
+//2: er noget (eg booleans?) en indikation paa at der skal tages tests?
 function App() {
     return (
         <div>
             <Header />
-            <MeasurementButton text="Mål Blodsukker" link="#TODO" />
+            
+            
+            <Routes>
+                <Route path="/" element={ <Home /> } />
+                <Route path="/measurebloodsugar" element={<BloodSugarForm />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </div>
     )
 }
---
+++ projectseraph.client/src/presentation/components/Home.tsx
@@ -0,0 +1,11 @@
+﻿import MeasurementButton from './MeasurementButton.tsx';
+
+function Home() {
+    return (
+        <div>
+            <MeasurementButton text="Mål Blodsukker" link="/measurebloodsugar" />
+        </div>
+    );
+}
+
+export default Home;
