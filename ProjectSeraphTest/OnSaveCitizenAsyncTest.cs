c7d4cf43e4a85b4d49e76b3ae263057813c26009
Niklas <jan-niklas-bremer@web.de>
2025-12-12 12:42:31 +0100

    Startet imple testing to project

---
+++ ProjectSeraphTest/OnSaveCitizenAsyncTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+
+namespace ProjectSeraphTest
+{
+    [TestClass]
+    public sealed class OnSaveCitizenAsyncTest
+    {
+        [TestMethod]
+        public void TestBranch1()
+        {
+        }
+
+        [TestMethod]
+        public void TestBranch2()
+        {
+        }
+
+        [TestMethod]
+        public void TestBranch3()
+        {
+        }
+
+        [TestMethod]
+        public void TestBranch4()
+        {
+        }
+
+        [TestMethod]
+        public void TestBranch5()
+        {
+        }
+
+    }
+}
