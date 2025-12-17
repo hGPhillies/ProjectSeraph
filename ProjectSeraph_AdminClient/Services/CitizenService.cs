95e0d39192c6f215883da131c809bd2d77b96040
Jepasej <146959451+Jepasej@users.noreply.github.com>
2025-12-17 10:55:04 +0100

    changed port to 5001

--- ProjectSeraph_AdminClient/Services/CitizenService.cs
+++ ProjectSeraph_AdminClient/Services/CitizenService.cs
@@ -18,7 +18,7 @@ namespace ProjectSeraph_AdminClient.Services
         {
             _http = new HttpClient
             {
-                BaseAddress = new Uri("https://localhost:5001")
+                BaseAddress = new Uri("http://localhost:5001")
             };
         }
 
