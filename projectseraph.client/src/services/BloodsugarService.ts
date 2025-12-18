781e3090946eaf0e65c799c89ab92d21fd2f8883
TobiasFalck <TobiasFalck13@gmail.com>
2025-12-18 09:55:07 +0100

    Webclient debug and fix

--- projectseraph.client/src/services/BloodsugarService.ts
+++ projectseraph.client/src/services/BloodsugarService.ts
@@ -8,7 +8,7 @@ import type { BloodsugarMeasurement } from "../domain/BloodsugarMeasurement";
 
 // REFACTOR
 //Ret URL, så vi er sikre på at den matcher backend-endpoint
-const BASE_URL = "https://localhost:5001/measurement/send/bloodsugar";
+const BASE_URL = "http://localhost:8080/measurement/send/bloodsugar";
 
 class BloodsugarService {
     static async sendMeasurement(measurement: BloodsugarMeasurement): Promise<void> {
